6eeb215935df8039b6d8154fae0ec41f4f086f2b
Nik <miniakink@gmail.com>
2020-07-04 01:23:49 +0300

    v2 My version

--- users/permission.py
+++ users/permission.py
@@ -2,6 +2,9 @@ from rest_framework import permissions
 
 
 class IsAdmin(permissions.BasePermission):
+    """
+    Request user id superuser or role is admin
+    """
     def has_permission(self, request, view):
         if request.user.is_superuser or request.user.role == 'admin':
             return True
